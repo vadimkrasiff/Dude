391dc9b0d2d2f69788b34950c23933575000131c
Vadim <krasilkoffvad99@gmail.com>
2023-01-23 16:04:06 +0500

    upgrade profile

--- src/component/Profile/Profile.jsx
+++ src/component/Profile/Profile.jsx
@@ -1,26 +1,25 @@
-import { EditOutlined, UserOutlined } from "@ant-design/icons"
-import { Button,  } from "antd"
-import { useRef } from "react"
+import { CloseOutlined, EditOutlined } from "@ant-design/icons"
+import { Button, Modal, } from "antd"
 import { useState } from "react"
 import { useEffect } from "react"
 import { connect } from "react-redux"
-import { useNavigate, useParams } from "react-router-dom"
+import {  useParams } from "react-router-dom"
 import { compose } from "redux"
-import Preloader from "../../comon/Preloader"
 import { withAuthRedirect } from "../../hoc/withAuthRedirect"
-import  { getUserProfile, savePhoto } from "../../redux/profile-reducer"
+import { getUserProfile, savePhoto } from "../../redux/profile-reducer"
 import css from "./Profile.module.css"
 import ProfileInfo from "./ProfileInfo"
 
 let Profile = (props) => {
 
-    let navigate = useNavigate();
-    let {id} = useParams();
+    let { id } = useParams();
     let userId = id;
-    let [isPhoto, setNewPhoto] = useState(false);
+    const [isPhoto, setNewPhoto] = useState(false);
+    const [open, setOpen] = useState(false);
+
 
     const refreshProfile = () => {
-        if(!userId){
+        if (!userId) {
             userId = props.authorizedUserId;
 
             props.getUserProfile(userId);
@@ -28,68 +27,92 @@ let Profile = (props) => {
     }
 
     const onMainPhotoSelected = (e) => {
-        if (e.target.files.length ){
-             props.savePhoto(e.target.files[0])
-             setNewPhoto(!isPhoto);
+        if (e.target.files.length) {
+            props.savePhoto(e.target.files[0])
+            setNewPhoto(!isPhoto);
         }
-      }
+    }
 
-    useEffect(()=> refreshProfile(), [userId])
+    useEffect(() => refreshProfile(), [userId])
 
-     
-    return <>{!props.profile  || props.isFetching  ? <div className={css.content}>
-    <div className={css.leftInfo}>
-        <div className={css.avatar}>
 
-        </div>
-        <Button type="primary" className={css.editButton} >
-            
-            <input className={css.inputFile} type="file"  ></input>
-            </Button>
-        
-    </div>
-    <div className={css.rightInfo}>
-        <div className={css.userInfo}>
-            <div className={css.titleInfo}>
-            </div>
-        </div>
-    </div>
-</div>  
-    : <div className={css.content}>
+    return <>{!props.profile || props.isFetching ? <div className={css.content}>
         <div className={css.leftInfo}>
             <div className={css.avatar}>
-                <div style={{
 
-                    backgroundImage: `url(${props.profile.photos.large})`,
-                    backgroundPosition: "center",
-                    height: "90%",
-                    width: "90%",
-                    borderRadius: 3,
-                    
-                }}>                    
-                </div>
             </div>
-            <Button type="primary" className={css.editButton} icon={<EditOutlined />}>
-                Edit photo
-                <input className={css.inputFile} type="file" onChange={onMainPhotoSelected} ></input>
-                </Button>
-            
+            <Button type="primary" className={css.editButton} >
+
+                <input className={css.inputFile} type="file"  ></input>
+            </Button>
+
         </div>
         <div className={css.rightInfo}>
-            <ProfileInfo profile={props.profile} />
+            <div className={css.userInfo}>
+                <div className={css.titleInfo}>
+                </div>
+            </div>
         </div>
-    </div>}
+    </div>
+        : <div className={css.content}>
+            <div className={css.leftInfo}>
+                <div className={css.avatar} >
+                    <div onClick={() => setOpen(true)} style={{
+
+                        backgroundImage: `url(${props.profile.photos.large})`,
+                        backgroundPosition: "center",
+                        height: "90%",
+                        width: "90%",
+                        borderRadius: 3,
+
+                    }}>
+                    </div>
+                    <Modal 
+
+                    footer={null}
+                        open={open}
+                        closable={false}
+                        onOk={() => setOpen(false)}
+                        onCancel={() => setOpen(false)}
+                        centered
+                        style={{height: 600}}
+                        bodyStyle={{
+                            position:"absolute",
+                            top: 0,
+                            left: 0,
+                            padding:0,
+                            backgroundImage: `url(${props.profile.photos.large})`,
+                            backgroundSize: "100% 100%",
+                            height: 600,
+                            width: 600,
+                            borderRadius: 5,
+                            
+                            }}>
+                        <Button onClick={() => setOpen(false)} type="text"
+                        style={{position: "absolute", right: "10px", top: "10px"}} icon={<CloseOutlined style={{color: "white", fontSize: "20px"}} />}></Button>
+                    </Modal>
+                </div>
+                <Button type="primary" className={css.editButton} icon={<EditOutlined />}>
+                    Edit photo
+                    <input className={css.inputFile} type="file" onChange={onMainPhotoSelected} ></input>
+                </Button>
+
+            </div>
+            <div className={css.rightInfo}>
+                <ProfileInfo profile={props.profile} />
+            </div>
+        </div>}
     </>
 }
 
 let mapStateToProps = (state) => {
     return ({
-      profile: state.profile.profile,
-      isFetching: state.profile.isFetching,
-      status: state.profile.status,
-      authorizedUserId: state.auth.userId,
-      isAuth: state.auth.isAuth
+        profile: state.profile.profile,
+        isFetching: state.profile.isFetching,
+        status: state.profile.status,
+        authorizedUserId: state.auth.userId,
+        isAuth: state.auth.isAuth
     })
-  }
+}
 
-export default compose(connect(mapStateToProps, {getUserProfile, savePhoto}) , withAuthRedirect)(Profile);
+export default compose(connect(mapStateToProps, { getUserProfile, savePhoto }), withAuthRedirect)(Profile);
--- src/component/Profile/ProfileInfo.jsx
+++ src/component/Profile/ProfileInfo.jsx
@@ -1,9 +1,15 @@
-import { EditOutlined } from "@ant-design/icons";
+import {  DownCircleOutlined, EditOutlined, UpCircleOutlined } from "@ant-design/icons";
 import { Button } from "antd";
 import React from "react";
+import { useState } from "react";
 import css from "./Profile.module.css"
 
 let ProfileInfo = ({ profile }) => {
+
+    const [isDeploy, setDeploy] = useState(false);
+
+    
+
     return <div className={css.userInfo}>
         <div className={css.titleInfo}>
             <div>
@@ -31,21 +37,45 @@ let ProfileInfo = ({ profile }) => {
         </div>
         <div className={css.partInfo}>
             <div className={css.titlePart}>Contacts</div>
-            <div>
+            <div className={!isDeploy ? css.contacts : css.fullContacts}>
                 <div className={css.userParam}>
-                    <div className={css.titleParam}>facebook:</div>
-                    <a href={profile.contacts.facebook} className={css.contentParam}>{profile.contacts.facebook}</a>
+                    <div className={css.titleParam}>Facebook:</div>
+                    <a href={profile.contacts.facebook || null} className={css.contentParam}>{profile.contacts.facebook || <div>no</div>}</a>
                 </div>
                 <div className={css.userParam}>
-                    <div className={css.titleParam}>Looking for a job:</div>
-                    <div className={css.contentParam}>{profile.lookingForAJob ? "yes" : "no"}</div>
+                    <div className={css.titleParam}>Website:</div>
+                    <a  href={profile.contacts.website || null} className={css.contentParam}>{profile.contacts.website || <div>no</div>}</a>
                 </div>
                 <div className={css.userParam}>
-                    <div className={css.titleParam}>Description:</div>
-                    <div className={css.contentParam}>{profile.lookingForAJobDescription}</div>
+                    <div className={css.titleParam}>VK:</div>
+                    <a href={profile.contacts.vk || null} className={css.contentParam}>{profile.contacts.vk || <div>no</div>}</a>
+                </div>
+                <div className={css.userParam}>
+                    <div className={css.titleParam}>Twitter:</div>
+                    <a href={profile.contacts.twitter || null} className={css.contentParam}>{profile.contacts.twitter || <div>no</div>}</a>
+                </div>
+                <div className={css.userParam}>
+                    <div className={css.titleParam}>Instagram:</div>
+                    <a  href={profile.contacts.instagram || null} className={css.contentParam}>{profile.contacts.instagram || <div>no</div>}</a>
+                </div>
+                <div className={css.userParam}>
+                    <div className={css.titleParam}>Youtube:</div>
+                    <a href={profile.contacts.youtube || null} className={css.contentParam}>{profile.contacts.youtube || <div>no</div>}</a>
+                </div>
+                <div className={css.userParam}>
+                    <div className={css.titleParam}>Github:</div>
+                    <a href={profile.contacts.github || null} className={css.contentParam}>{profile.contacts.github || <div>no</div>}</a>
+                </div>
+                <div className={css.userParam}>
+                    <div className={css.titleParam}>MainLink:</div>
+                    <a href={profile.contacts.mainLink || null} className={css.contentParam}>{profile.contacts.mainLink || <div>no</div>}</a>
                 </div>
             </div>
         </div>
+        <Button className={css.buttonDeploy} onClick={() => setDeploy(!isDeploy)} type="text">
+            {!isDeploy? <div>Подробнее <DownCircleOutlined /></div> : <div>Скрыть <UpCircleOutlined /></div>} 
+            </Button>
+
     </div>
 }
 
